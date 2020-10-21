51e460c6d98bce0b7725246796bc8c66bbed3742
afsanarozan <afsanarozannaufal9@gmail.com>
2020-10-21 20:07:51 +0700

    add demo-pgadmin

--- src/model/user.js
+++ src/model/user.js
@@ -3,7 +3,8 @@ const user = {}
 
 user.Get = () => {
     return new Promise((resolve, reject) => {
-        database.query(`SELECT * FROM login`)
+        database.query
+        (`SELECT * FROM "login"`)
         .then(res => {
             resolve(res.rows)
         })
